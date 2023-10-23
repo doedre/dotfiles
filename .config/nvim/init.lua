9968b073a188a4d72bc19ab88e6cda3f48e18809
Anton Shevelyov <Anton.Shevelyov@hotmail.com>
2023-10-23 15:13:15 +0600

    Clean and update nvim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,185 +1,267 @@
-local cmd = vim.cmd             -- Execute Vim commands
-local exec = vim.api.nvim_exec  -- execute Vimscript
-local g = vim.g                 -- global variables
-local opt = vim.opt             -- global/buffer/window-scoped options
-
--- Package manager and plugins to install
-require "paq" {
-  "savq/paq-nvim",                        -- Let Paq manage itself
-  "nvim-treesitter/nvim-treesitter",      -- Better syntax parsing
-  "itchyny/lightline.vim",                -- Light and configurable statusline
-  "lewis6991/gitsigns.nvim",              -- Git decorations and utilities 
-  "unblevable/quick-scope",               -- Highlight unique characters in current line
-  "huyvohcmc/atlas.vim",                  -- Minimal colorscheme (no accent color)
-  "ewilazarus/preto",                     -- Minimal colorscheme (more shades of gray)
-  "kadekillary/skull-vim",                -- Minimal colorscheme (nice green accent)
-  "ellisonleao/gruvbox.nvim",             -- gruvbox theme
-
-  "lervag/vimtex",                        -- TeX support
-
-  "nvim-lua/plenary.nvim",                -- Telescope dependency
-  "nvim-telescope/telescope.nvim",        -- Fuzzy finder
-  "nvim-telescope/telescope-fzy-native.nvim" ,  -- Sorter for Telescope
-
-  "neovim/nvim-lspconfig",                -- LSP
-  "nvim-lua/lsp-status.nvim",             -- Status for LSP
-
-  "hrsh7th/cmp-nvim-lsp",                 -- Completion of code
-  "hrsh7th/cmp-buffer",                   -- 
-  "hrsh7th/cmp-path",                     -- Path completion
-  "hrsh7th/cmp-cmdline",
-  "hrsh7th/nvim-cmp",
-  "dcampos/nvim-snippy",
-  "dcampos/cmp-snippy",
-}
-
-require('nvim-treesitter.configs').setup {
-  -- A list of parser names ("all" for everyone)
-  ensure_installed = { "c", "cpp", "rust", "lua", "hare" },
-
-  -- Automatically install missing parsers when entering buffer
-  auto_install = false,
-
-  highlight = {
-    enable = true,
-
-    -- List of parsers to disable
-    -- disable = {},
-
-    -- Additional highlight with regexp rules for specified languages
-    -- additional_vim_regex_highlighting = {}
-  },
-}
-
-require('telescope').setup {
-  extensions = {
-    fzy_native = {
-      override_generic_sorter = false,
-      override_file_sorter = true,
-    }
-  }
-}
-
-require('telescope').load_extension('fzy_native')
-
---[[
-require('snippy').setup {
-    mappings = {
-        is = {
-            ['<Tab>'] = 'expand_or_advance',
-            ['<S-Tab>'] = 'previous',
-        },
-        nx = {
-            ['<leader>x'] = 'cut_text',
-        },
-    },
-}
---]]
-
-local cmp = require('cmp')
-local snippy = require("snippy")
-local lspconfig = require('lspconfig')
-local lsp_status = require('lsp-status')
-
-local has_words_before = function()
-  unpack = unpack or table.unpack
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+-- Bootstrap lazy.nvim plugin manager
+--
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
 
-cmp.setup {
+vim.opt.rtp:prepend(lazypath)
+
+-- Setup plugin manager
+--
+require("lazy").setup({
+  -- Colorschemes
+  "ellisonleao/gruvbox.nvim",
+  "kxzk/skull-vim",
+  -- Treesitter for better code parsing
+  "nvim-treesitter/nvim-treesitter",
+  -- Code completion and LSP
+  "neovim/nvim-lspconfig",
+  "nvim-lua/lsp-status.nvim",
+  "hrsh7th/cmp-nvim-lsp",
+  "hrsh7th/cmp-buffer",
+  "hrsh7th/cmp-path",
+  "hrsh7th/nvim-cmp",
+  -- Snippets
+  "L3MON4D3/LuaSnip",
+  -- Statusline
+  "nvim-lualine/lualine.nvim",
+  -- Git signs
+  "lewis6991/gitsigns.nvim",
+})
+
+-- General settings
+--
+vim.highlight.on_yank = true
+vim.o.mouse = 'a'
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.textwidth = 80
+vim.o.cursorline = true
+vim.o.cursorcolumn = false
+vim.o.colorcolumn = '80'
+vim.o.signcolumn = 'yes'
+vim.o.title = true
+vim.o.scrolloff = 3
+vim.o.termguicolors = true
+vim.opt.expandtab = false
+vim.opt.tabstop = 8
+vim.opt.shiftwidth = 8
+vim.opt.smartindent = true
+vim.cmd("set nofoldenable")
+vim.cmd("let g:c_syntax_for_h = 1")
+
+-- Colorscheme
+--
+require("gruvbox").setup {
+  undercurl = true,
+  underline = true,
+  bold = true,
+  italic = {
+    strings = true,
+    comments = true,
+    operators = false,
+    folds = true,
+  },
+  strikethrough = true,
+  invert_selection = false,
+  invert_signs = false,
+  invert_tabline = false,
+  invert_intend_guides = false,
+  inverse = true, -- invert background for search, diffs, statuslines and errors
+  contrast = "soft", -- can be "hard", "soft" or empty string
+  palette_overrides = {},
+  overrides = {},
+  dim_inactive = false,
+  transparent_mode = false,
+}
+vim.cmd("colorscheme gruvbox")
+
+-- Code completion settings
+--
+vim.o.completeopt = 'menu,menuone,noselect'
+
+local luasnip = require("luasnip")
+local cmp = require("cmp")
+
+cmp.setup({
   snippet = {
     expand = function(args)
-      snippy.expand_snippet(args.body)
+      luasnip.lsp_expand(args.body)
     end,
   },
-  window = {
-    -- completion = cmp.config.window.bordered(),
-    documentation = cmp.config.window.bordered(),
-  },
   mapping = cmp.mapping.preset.insert({
-    ["<Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif snippy.can_expand_or_advance() then
-        snippy.expand_or_advance()
-      elseif has_words_before() then
-        cmp.complete()
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
-
-    ["<S-Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif snippy.can_jump(-1) then
-        snippy.previous()
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
-
-    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-e>'] = cmp.mapping.abort(),
-    ['<CR>'] = cmp.mapping.confirm({ select = false }),
+    ['<CR>'] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Replace,
+    },
+    ['<C-e>'] = cmp.mapping({
+      i = cmp.mapping.abort(),
+      c = cmp.mapping.close(),
+    }),
+    ['<Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+    ['<S-Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
   }),
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
-    { name = 'snippy' },
-    { name = 'buffer' }
+    { name = 'luasnip' }
+  }, {
+    { name = 'buffer' },
+    { name = 'path' }
   })
-}
+})
 
-cmp.setup.cmdline({ '/', '?' }, {
-  mapping = cmp.mapping.preset.cmdline(),
-  sources = {
-    { name = 'buffer' }
+-- LSP configuration
+--
+local lspconfig = require("lspconfig")
+
+-- Mappings.
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+local opts = { noremap = true, silent = true }
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
+
+-- Use an on_attach function to only map the following keys
+-- after the language server attaches to the current buffer
+local on_attach = function(client, bufnr)
+  -- Enable completion triggered by <c-x><c-o>
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+  -- Mappings.
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  local bufopts = { noremap = true, silent = true, buffer = bufnr }
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+  vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+  vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+  vim.keymap.set('n', '<space>wl', function()
+    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  end, bufopts)
+  vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+  vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+  vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+  vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
+end
+
+-- Add additional capabilities supported by nvim-cmp
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
+
+-- Use a loop to conveniently call 'setup' on multiple servers and
+-- map buffer local keybindings when the language server attaches
+local servers = { 'clangd', 'pylsp', 'rust_analyzer' }
+for _, lsp in pairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = on_attach,
+    capabilities = capabilities
+  }
+end
+
+-- Statusline configuration
+--
+require("lualine").setup({
+  options = {
+    theme = require("lualine.themes.gruvbox"),
+    section_separators = '',
+    component_separators = ''
   }
 })
 
-cmp.setup.cmdline(':', {
-  mapping = cmp.mapping.preset.cmdline(),
-  sources = cmp.config.sources({
-    { name = 'path' },
-    { name = 'cmdline' }
-  })
-})
+-- Treesitter configuration
+--
+require("nvim-treesitter.configs").setup({
+  ensure_installed = { "c", "cpp", "rust", "lua", "hare" },
 
-local cmp_capabilities = require('cmp_nvim_lsp').default_capabilities()
+  -- Automatically install missing parsers when entering buffer
+  auto_install = true,
 
-lspconfig.clangd.setup {
-  handlers = lsp_status.extensions.clangd.setup(),
-  init_options = {
-    clangdFileStatus = true
+  highlight = {
+    enable = true,
   },
-  on_attach = lsp_status.on_attach,
-  capabilities = {
-    lsp_status.capabilities,
-    cmp_capabilities
-  }
-}
 
-lspconfig.neocmake.setup {
-  on_attach = lsp_status.on_attach,
-  capabilities = lsp_status.capabilities
-}
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = "gnn",
+      node_incremental = "grn",
+      scope_incremental = "grc",
+      node_decremental = "grm",
+    },
+  },
+  indent = { enable = false }
+})
 
-lspconfig.rust_analyzer.setup {
-  on_attach = lsp_status.on_attach,
-  capabilities = lsp_status.capabilities
-}
+-- Setup folding based on treesitter
+vim.o.foldmethod = 'expr'
+vim.o.foldexpr = 'nvim_treesitter#foldexpr()'
+vim.o.foldnestmax = 1
 
+-- Git signs configuration
+-- 
 require('gitsigns').setup {
   -- Signs and their colors
   signs = {
-    add             = {hl = 'GitSignsAdd'   , text = '▌', numhl='GitSignsAddNr'   , linehl='GitSignsAddLn'},
-    change          = {hl = 'GitSignsChange', text = '▌', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
-    delete          = {hl = 'GitSignsDelete', text = '▁', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
-    topdelete       = {hl = 'GitSignsDelete', text = '▔', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
-    changedelete    = {hl = 'GitSignsChange', text = '▌', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
+    add = {
+      hl = 'GitSignsAdd',
+      text = '▌',
+      numhl='GitSignsAddNr',
+      linehl='GitSignsAddLn'
+    },
+    change = {
+      hl = 'GitSignsChange',
+      text = '▌',
+      numhl='GitSignsChangeNr',
+      linehl='GitSignsChangeLn'
+    },
+    delete = {
+      hl = 'GitSignsDelete',
+      text = '▁',
+      numhl='GitSignsDeleteNr',
+      linehl='GitSignsDeleteLn'
+    },
+    topdelete = {
+      hl = 'GitSignsDelete',
+      text = '▔',
+      numhl='GitSignsDeleteNr',
+      linehl='GitSignsDeleteLn'
+    },
+    changedelete = {
+      hl = 'GitSignsChange',
+      text = '▌',
+      numhl='GitSignsChangeNr',
+      linehl='GitSignsChangeLn'
+    },
   },
 
   signcolumn = true,  -- Toggle with `:Gitsigns toggle_signs`
@@ -193,7 +275,7 @@ require('gitsigns').setup {
   },
 
   attach_to_untracked = true,
-  current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+  current_line_blame = true,  -- Toggle with `:Gitsigns toggle_current_line_blame`
   current_line_blame_opts = {
     virt_text = true,
     virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
@@ -221,72 +303,5 @@ require('gitsigns').setup {
   },
 }
 
-require('gruvbox').setup {
-  undercurl = true,
-  underline = true,
-  bold = true,
-  italic = {
-    strings = true,
-    comments = true,
-    operators = false,
-    folds = true,
-  },
-  strikethrough = true,
-  invert_selection = false,
-  invert_signs = false,
-  invert_tabline = false,
-  invert_intend_guides = false,
-  inverse = true, -- invert background for search, diffs, statuslines and errors
-  contrast = "soft", -- can be "hard", "soft" or empty string
-  palette_overrides = {},
-  overrides = {},
-  dim_inactive = false,
-  transparent_mode = false,
-}
-
--- Main options
-opt.cursorline = true           -- Cursor line highlight
-opt.colorcolumn = '80'          -- Vertical line
-opt.number = true               -- Number column on the left
-opt.hidden = true               -- Allow switching without saving
-opt.signcolumn = 'yes'          -- Additional line for signs on the left is always visible
-opt.relativenumber = true
-
--- Identation settings
-opt.expandtab = false
-opt.tabstop = 8
-opt.smartindent = true
-
-g.vimtex_view_method = 'zathura'
-
-cmd([[
-syntax enable
-set termguicolors 
-colorscheme skull
-
-let g:lightline = { 'colorscheme': 'atlas' }
-let g:c_syntax_for_h = 1
-
-let g:qs_highlight_on_keys = ['f', 'F', 't', 'T']
-let g:qs_max_chars=200
-highlight QuickScopePrimary guifg='#7ede81' gui=underline ctermfg=155 cterm=underline
-highlight QuickScopeSecondary guifg='#ffffff' gui=underline ctermfg=81 cterm=underline
- 
-let g:vimtex_view_method = 'zathura'
-let g:vimtex_compiler_latexmk = {
-\ 'build_dir': 'output',
-\}
-
-autocmd BufRead,BufNewFile *.cppm set filetype=cpp
-autocmd BufRead,BufNewFile *.cppp set filetype=cpp
-autocmd BufRead,BufNewFile *.ixx set filetype=cpp
-
-function! LspStatus() abort
-  if luaeval('#vim.lsp.buf_get_clients() > 0')
-    return luaeval("require('lsp-status').status()")
-  endif
-
-  return ''
-endfunction
-]])
+vim.keymap.set('n', '<space>ghr', ':Gitsigns reset_hunk<CR>', opts)
 
