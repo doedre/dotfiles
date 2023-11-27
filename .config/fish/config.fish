93861cd56fc2c33bc8dec3699503dce082b3fcec
Anton Shevelyov <Anton.Shevelyov@hotmail.com>
2023-11-27 22:36:51 +0600

    Add fish configuration files

---
+++ .config/fish/config.fish
@@ -0,0 +1,6 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# opam configuration
+source /home/anton/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
