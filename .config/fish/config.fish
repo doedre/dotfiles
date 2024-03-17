cb0ecee802b177ce3857684510c6964f939454b3
Anton Shevelyov <Anton.Shevelyov@hotmail.com>
2024-03-17 11:00:20 +0600

    Add zellij layout for CMake, update env vars

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,10 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+    set -U fish_greeting
 end
 
 # opam configuration
 source /home/anton/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
+
+set -gx EDITOR nvim
+set -gx VISUAL less
+
