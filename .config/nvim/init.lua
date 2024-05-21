1b8f6f44028656ba899c68af819c237cf6330e76
Anton Shevelyov <Anton.Shevelyov@hotmail.com>
2024-05-21 15:20:30 +0600

    update configs

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -187,7 +187,7 @@ capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
-local servers = { 'clangd', 'pylsp', 'rust_analyzer', 'zls' }
+local servers = { 'clangd', 'pylsp', 'rust_analyzer', 'zls', 'ols' }
 for _, lsp in pairs(servers) do
   lspconfig[lsp].setup {
     on_attach = on_attach,
